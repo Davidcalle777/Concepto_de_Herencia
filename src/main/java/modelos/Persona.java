5a258d454120243a7e48103afe0d4c605f9b9193
Davidcalle777 <david.calle@est.iudigital.edu.co>
2025-05-30 10:57:07 -0500

    En persona se Cambio: Se creó o se ajustó como clase padre para manejar atributos comunes: nombre, apellido, fechaNacimiento, genero, estatura, peso.Importante: Se centraliza atributos compartidos por todos los tipos de personas.

--- src/main/java/modelos/Persona.java
+++ src/main/java/modelos/Persona.java
@@ -1,4 +1,45 @@
-package modelos;
 
+package modelos;
+// Importa las clases necesarias para manejar la herencia y atributos de Persona
 public class Persona {
+    protected String nombre;
+    protected String apellido;
+    protected String fechaNacimiento;
+    protected String genero;
+    protected double estatura;
+    protected double peso;
+    // Constructor de la clase Persona
+    public Persona(String nombre, String apellido, String fechaNacimiento, String genero,
+                   double estatura, double peso) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNacimiento = fechaNacimiento;
+        this.genero = genero;
+        this.estatura = estatura;
+        this.peso = peso;
+    }
+    // Métodos getters para acceder a los atributos de la clase Persona
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public double getEstatura() {
+        return estatura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
 }
